53cac03ab22062fce03a051c1dadd30818b39188
aube.lee <leejjoo112@icloud.com>
2024-10-26 15:19:33 +0900

    project init

---
+++ PROJECT-MM/Assets/DebugTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTest : MonoBehaviour
+{
+    private void Awake ()
+    {
+        Debug.Log("안녕!!!");
+    }
+}
