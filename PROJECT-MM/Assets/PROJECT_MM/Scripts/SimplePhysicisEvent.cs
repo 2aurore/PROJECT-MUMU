02e05488f10627baf63b63a3d26c92e6b490d4a1
aube.lee <leejjoo112@icloud.com>
2024-10-29 23:06:27 +0900

    Instantiate 테스트 코드 추가

--- PROJECT-MM/Assets/PROJECT_MM/Scripts/SimplePhysicisEvent.cs
+++ PROJECT-MM/Assets/PROJECT_MM/Scripts/SimplePhysicisEvent.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SimplePhysicisEvent : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject cubePrefab;
+
     private void OnCollisionEnter(Collision collision) 
     {
         Debug.Log("Collision Enter : "+ collision.gameObject.name);
@@ -22,10 +25,21 @@ public class SimplePhysicisEvent : MonoBehaviour
     {
         Debug.Log("Trigger Enter : "+ other.gameObject.name);
 
+        if (other.gameObject.name == "Capsule")
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Instantiate(cubePrefab);
+            }
+                Destroy(cubePrefab, 10f);
+            
+        }
     }
+
     private void OnTriggerStay(Collider other) 
     {
         Debug.Log("Trigger Stay : "+ other.gameObject.name);
+
 
     }
     private void OnTriggerExit(Collider other) 
