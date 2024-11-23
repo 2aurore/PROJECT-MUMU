f75119fa415007299b2f2ee79ccf99f6177b8c36
aube.lee <leejjoo112@icloud.com>
2024-11-23 15:48:15 +0900

    left ctrl 키 입력시 캐릭터 standing/crouching 동작 전환 로직 추가

--- PROJECT-MM/Assets/PROJECT_MM/Scripts/Charactor/CharactorController.cs
+++ PROJECT-MM/Assets/PROJECT_MM/Scripts/Charactor/CharactorController.cs
@@ -7,6 +7,7 @@ public class CharactorController : MonoBehaviour
     public float moveSpeed = 2f;
     public Animator animator;
     public bool isRunning = false;
+    public bool isCrouching = false;
 
     private void Awake() {
         animator = GetComponent<Animator>();
@@ -19,15 +20,20 @@ public class CharactorController : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         isRunning = Input.GetKey(KeyCode.LeftShift);
-        isStanding = Input.GetKey(KeyCode.LeftControl);
-        // 캐릭터가 달리는 중일 때 속도를 높이게 함
-        float dynamicMoveSpeed = isRunning ? moveSpeed * 2 : moveSpeed;
+
+        // left control 키가 눌렸을때 상태를 전환
+        if (Input.GetKeyDown(KeyCode.LeftControl)) {
+            isCrouching = !isCrouching;
+        }
+        // 캐릭터가 달리는 중일 때 속도를 높이게 하고, 자세를 숙인 상태에서 이동속도 절반으로 줄임
+        float dynamicMoveSpeed = isRunning ? moveSpeed * 2 : isCrouching ? moveSpeed / 2 : moveSpeed;
 
         Vector2 input = new Vector2(horizontal, vertical);
 
         animator.SetFloat("Speed", input.sqrMagnitude > 0f ? isRunning ? 3f : 0.5f : 0f);
         animator.SetFloat("Horizontal", horizontal);
         animator.SetFloat("Vertical", vertical);
+        animator.SetBool("Crouching", isCrouching);
         
         Vector3 movement = new Vector3(horizontal, 0f, vertical);
         transform.Translate(movement * dynamicMoveSpeed * Time.deltaTime, Space.Self);
