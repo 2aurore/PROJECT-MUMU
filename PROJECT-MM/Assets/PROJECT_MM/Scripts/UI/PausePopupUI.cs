feeae75c2ece28a851b2364ea355e0ed23aa5b18
aube.lee <leejjoo112@icloud.com>
2024-12-06 18:00:49 +0900

    PausePopupUI 내 게임 종료 버튼 추가

--- PROJECT-MM/Assets/PROJECT_MM/Scripts/UI/PausePopupUI.cs
+++ PROJECT-MM/Assets/PROJECT_MM/Scripts/UI/PausePopupUI.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PausePopupUI : UIBase
 {
+    // 게임 일시정지 버튼 메소드
     public void OnClickResumeButton()
     {
         UIManager.Hide<PausePopupUI>(UIList.PausePopupUI);
+    }
+
+
+    // 게임 종료 버튼 메소드
+    public void OnClickExitButton()
+    {
+        Application.Quit();
+        EditorApplication.isPlaying = false;    // 엔진에서 게임을 종료 처리하도록 함
     }
 }
