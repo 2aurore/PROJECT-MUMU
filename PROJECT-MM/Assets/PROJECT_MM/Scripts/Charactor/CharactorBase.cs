573fb159e8438d2ca22ee95f749d81819014c7e9
aube.lee <leejjoo112@icloud.com>
2024-12-16 21:55:45 +0900

    WaitForSecondsRealtime 로 실제 리얼 타임으로 yield 리턴 변경

--- PROJECT-MM/Assets/PROJECT_MM/Scripts/Charactor/CharactorBase.cs
+++ PROJECT-MM/Assets/PROJECT_MM/Scripts/Charactor/CharactorBase.cs
@@ -305,7 +305,7 @@ public class CharactorBase : MonoBehaviour, IDamage
   IEnumerator SlowMotion()
   {
     Time.timeScale = 0.5f;
-    yield return new WaitForSeconds(3.0f);
+    yield return new WaitForSecondsRealtime(3.0f);
     Time.timeScale = 1f;
   }
 }
